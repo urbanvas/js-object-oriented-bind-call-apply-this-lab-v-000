bd88bfbc2068f08415ad473876498ee53bbe6e3d
Neil Seejoor <nvseejoor@gmail.com>
2018-10-10 01:00:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function justInvoke(fn) {
 function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, [args])
+}
