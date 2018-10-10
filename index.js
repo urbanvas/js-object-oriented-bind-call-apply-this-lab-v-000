37817cc81c8862e72d567377d583ebbcc9903808
Neil Seejoor <nvseejoor@gmail.com>
2018-10-10 01:01:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
 function setThisWithApply(fn, thisValue, args) {
-  return fn.apply(thisValue, [args])
+  return fn.apply(thisValue, args)
 }
